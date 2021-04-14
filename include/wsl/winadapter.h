1c7cd82a6d2b34d514f727f3ddbcd7389459b4aa
Justin Stoecker <justoeck@microsoft.com>
2021-04-13 18:24:07 -0700

    Typedef for ULONG_PTR

--- include/wsl/winadapter.h
+++ include/wsl/winadapter.h
@@ -22,7 +22,7 @@ typedef uint16_t UINT16;
 typedef int16_t INT16;
 typedef uint32_t UINT32, UINT, ULONG, DWORD, BOOL;
 typedef int32_t INT32, INT, LONG;
-typedef uint64_t UINT64;
+typedef uint64_t UINT64, ULONG_PTR;
 typedef int64_t INT64, LONG_PTR;
 typedef void VOID, *HANDLE, *RPC_IF_HANDLE, *LPVOID;
 typedef const void *LPCVOID;
