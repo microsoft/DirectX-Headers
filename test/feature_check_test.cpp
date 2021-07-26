9aed6f37f9ac7a2a77901a520e015126dd03c127
Yizhou Sheng <t-yisheng@microsoft.com>
2021-07-26 16:14:55 -0700

    Feature Support API (Part 2) (#2)
    * Implement the second half of feature checks and update the sanity test.
    
    * Squashed commit of the following:
    
    commit a917854d2b2d828f6d774f165e59bca4544ceae4
    Author: Yizhou Sheng <t-yisheng@microsoft.com>
    Date:   Thu Jul 8 18:01:57 2021 -0700
    
        Optimized procedure to check Architecture support. Removed m_hStatus assignment in macros.
    
    * Updated initialization procedures
    
    * Remove duplicate checks.
    
    * Remove outdated comments and unnecessary test setup.
    
    * Rename loop variables. Fix duplicate cap check logic.
    
    - Fixed an issue with caps that exist in more than one feature. See the following comments for details.
    - Renamed loop variables to be more descriptive.
    - Use TIER_0 as default values for applicable caps.
    - Minor format fix.
    
    * Remove redundant functions and constructors. Reformatted code
    
    - Kept only the default constructor and the Init() function. Removed Create() and the customized constructor.
    - Removed a duplicate SharedResourceCompatibilityTier getter function from the Displayable feature.
    - Moved opening curly braces to new lines.
    - Separate function declaration from definition. Moved definition out of the class.
    - Replaced some macros with actual code.
    - Updated basic test files.

--- test/feature_check_test.cpp
+++ test/feature_check_test.cpp
@@ -5,9 +5,6 @@
 #include <wsl/winadapter.h>
 #endif
 
-#if ((__cplusplus >= 201703L) || _MSVC_LANG >= 201703L)
-#define FEATURE_SUPPORT_USE_VARIANT
-#endif
 #include <directx/d3d12.h>
 #include <directx/dxcore.h>
 #include <directx/d3dx12.h>
@@ -15,98 +12,144 @@
 
 int main()
 {
-    IDXCoreAdapter *adapter = nullptr;
     ID3D12Device *device = nullptr;
 
+    if (FAILED(D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device)))) 
     {
-        IDXCoreAdapterFactory *factory = nullptr;
-        if (FAILED(DXCoreCreateAdapterFactory(&factory)))
-            return -1;
-
-        IDXCoreAdapterList *list = nullptr;
-        if (FAILED(factory->CreateAdapterList(1, &DXCORE_ADAPTER_ATTRIBUTE_D3D12_CORE_COMPUTE, &list)))
-            return -1;
-        
-        if (FAILED(list->GetAdapter(0, &adapter)))
-            return -1;
-    }
-
-    if (FAILED(D3D12CreateDevice(adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&device)))) {
         return -1;
     }
 
-#ifdef FEATURE_SUPPORT_USE_VARIANT
-     CD3DX12FeatureSupport* features = nullptr;
-     auto features_requested = CD3DX12FeatureSupport::Create(device);
-     if (auto failure = std::get_if<HRESULT>(&features_requested)) {
-         return -1;
-     }
-
-     features = std::get_if<CD3DX12FeatureSupport>(&features_requested);
-     if (!features) {
-         return -1;
-     }
-    // CD3DX12FeatureSupport* features = new CD3DX12FeatureSupport(device);
-    if (FAILED(features->GetStatus())) {
+    CD3DX12FeatureSupport features;
+    if (FAILED(features.Init(device)))
+    {
         return -1;
     }
-#else
-    CD3DX12FeatureSupport feature_struct = CD3DX12FeatureSupport::Create(device);
-    if (FAILED(feature_struct.GetStatus())) {
-        return -1;
-    }
-    CD3DX12FeatureSupport* features = &feature_struct;
-#endif
 
     // 0: D3D12_OPTIONS
-    BOOL DoublePrecisionFloatShaderOps = features->DoublePrecisionFloatShaderOps();
-    D3D12_CONSERVATIVE_RASTERIZATION_TIER ConservativeRasterizationTier = features->ConservativeRasterizationTier();
+    BOOL DoublePrecisionFloatShaderOps = features.DoublePrecisionFloatShaderOps();
+    D3D12_CONSERVATIVE_RASTERIZATION_TIER ConservativeRasterizationTier = features.ConservativeRasterizationTier();
 
     // 2: Feature Levels
-    D3D_FEATURE_LEVEL HighestLevelSupported = features->HighestFeatureLevel();
+    D3D_FEATURE_LEVEL HighestLevelSupported = features.HighestFeatureLevel();
 
     // 3: Format Support
     D3D12_FORMAT_SUPPORT1 Support1;
     D3D12_FORMAT_SUPPORT2 Support2;
-    if (FAILED(features->FormatSupport(DXGI_FORMAT_R16G16B16A16_TYPELESS, Support1, Support2))) {
+    if (FAILED(features.FormatSupport(DXGI_FORMAT_R16G16B16A16_TYPELESS, Support1, Support2))) {
         return -1;
     }
 
     // 4: Multisample Quality Support
     UINT NumQualityLevels;
-    if (FAILED(features->MultisampleQualityLevels(DXGI_FORMAT_R16G16B16A16_TYPELESS, 1, D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE, NumQualityLevels))) {
+    if (FAILED(features.MultisampleQualityLevels(DXGI_FORMAT_R16G16B16A16_TYPELESS, 1, D3D12_MULTISAMPLE_QUALITY_LEVELS_FLAG_NONE, NumQualityLevels))) {
         return -1;
     }
 
     // 5: Format Info
     UINT8 PlaneCount;
-    if (FAILED(features->FormatInfo(DXGI_FORMAT_R16G16B16A16_TYPELESS, PlaneCount))) {
+    if (FAILED(features.FormatInfo(DXGI_FORMAT_R16G16B16A16_TYPELESS, PlaneCount))) {
         return -1;
     }
 
     // 6: GPU Virtual Address Support
-    UINT MaxGPUVABitsPerProcess = features->MaxGPUVirtualAddressBitsPerProcess();
-    UINT MaxGPUVABitsPerResource = features->MaxGPUVirtualAddressBitsPerResource();
+    UINT MaxGPUVABitsPerProcess = features.MaxGPUVirtualAddressBitsPerProcess();
+    UINT MaxGPUVABitsPerResource = features.MaxGPUVirtualAddressBitsPerResource();
 
     // 7: Shader Model
-    D3D_SHADER_MODEL HighestShaderModel = features->HighestShaderModel();
+    D3D_SHADER_MODEL HighestShaderModel = features.HighestShaderModel();
     
     // 8: Options1
-    BOOL WaveOps = features->WaveOps();
-    UINT WaveLaneCountMin = features->WaveLaneCountMin();
-    UINT WaveLaneCountMax = features->WaveLaneCountMax();
+    BOOL WaveOps = features.WaveOps();
+    UINT WaveLaneCountMin = features.WaveLaneCountMin();
+    UINT WaveLaneCountMax = features.WaveLaneCountMax();
 
     // 10: Protected Resource Session Support
-    D3D12_PROTECTED_RESOURCE_SESSION_SUPPORT_FLAGS ProtectedResourceSessionSupport = features->ProtectedResourceSessionSupport();
+    D3D12_PROTECTED_RESOURCE_SESSION_SUPPORT_FLAGS ProtectedResourceSessionSupport = features.ProtectedResourceSessionSupport();
 
     // 12: Root Signature
-    D3D_ROOT_SIGNATURE_VERSION HighestRootSignatureVersion = features->HighestRootSignatureVersion();
+    D3D_ROOT_SIGNATURE_VERSION HighestRootSignatureVersion = features.HighestRootSignatureVersion();
 
     // 16: Architecture1
-    BOOL TileBasedRenderer = features->TileBasedRenderer();
-    BOOL UMA = features->UMA();
-    BOOL CacheCoherentUMA = features->CacheCoherentUMA();
-    BOOL IsolatedMMU = features->IsolatedMMU();
+    BOOL IsolatedMMU = features.IsolatedMMU();
+    BOOL TileBasedRenderer = features.TileBasedRenderer();
+    BOOL UMA = features.UMA();
+    BOOL CacheCoherentUMA = features.CacheCoherentUMA();
+
+    // 18: Options2
+    BOOL DepthBoundsTestSupported = features.DepthBoundsTestSupported();
+    D3D12_PROGRAMMABLE_SAMPLE_POSITIONS_TIER ProgrammableSamplePositionsTier = features.ProgrammableSamplePositionsTier();
+
+    // 19: Shader Cache
+    D3D12_SHADER_CACHE_SUPPORT_FLAGS ShaderCacheSupportFlags = features.ShaderCacheSupportFlags();
+
+    // 20: Command Queue Prioirity
+    BOOL CommandQueuePrioritySupportedPositive = features.CommandQueuePrioritySupported(D3D12_COMMAND_LIST_TYPE_DIRECT, 0);
+    BOOL CommandQueuePrioritySupportedNegative = features.CommandQueuePrioritySupported(D3D12_COMMAND_LIST_TYPE_COPY, D3D12_COMMAND_QUEUE_PRIORITY_GLOBAL_REALTIME);
+    BOOL CommandQueuePrioritySupportedInvalid = features.CommandQueuePrioritySupported((D3D12_COMMAND_LIST_TYPE)7, 0);
+    
+    // 21: Options3
+    BOOL CopyQueueTimestampQueriesSupported = features.CopyQueueTimestampQueriesSupported();
+    BOOL CastingFullyTypedFormatSupported = features.CastingFullyTypedFormatSupported();
+    D3D12_COMMAND_LIST_SUPPORT_FLAGS WriteBufferImmediateSupportFlags = features.WriteBufferImmediateSupportFlags();
+    D3D12_VIEW_INSTANCING_TIER ViewInstancingTier = features.ViewInstancingTier();
+    BOOL BarycentricsSupported = features.BarycentricsSupported();
+
+    // 22: Existing Heaps
+    BOOL ExistingHeapsSupported = features.ExistingHeapsSupported();
+
+    // 23: D3D12 Options4
+    BOOL MSAA64KBAlgnedTextureSupported = features.MSAA64KBAlignedTextureSupported();
+    D3D12_SHARED_RESOURCE_COMPATIBILITY_TIER SharedResourceCompatibilityTier = features.SharedResourceCompatibilityTier();
+    BOOL Native16BitShaderOpsSupported = features.Native16BitShaderOpsSupported();
+
+    // 24: Serialization
+    D3D12_HEAP_SERIALIZATION_TIER HeapSerializationTier = features.HeapSerializationTier();
+
+    // 25: Cross Node
+    //  D3D12_CROSS_NODE_SHARING_TIER CrossNodeSharingTier = features.CrossNodeSharingTier(); // Same option in D3D12Options
+    BOOL CrossNodeAtomicShaderInstructions = features.CrossNodeAtomicShaderInstructions();
+
+    // 27: Options5
+    BOOL SRVOnlyTiledResourceTier3 = features.SRVOnlyTiledResourceTier3();
+    D3D12_RENDER_PASS_TIER RenderPassesTier = features.RenderPassesTier();
+    D3D12_RAYTRACING_TIER RaytracingTier = features.RaytracingTier();
+
+    // 28: Displayable
+    BOOL DisplayableTexture = features.DisplayableTexture();
+
+    // 30: Options6
+    BOOL AdditionalShadingRatesSupported = features.AdditionalShadingRatesSupported();
+    BOOL PerPrimitiveShadingRateSupportedWithViewportIndexing = features.PerPrimitiveShadingRateSupportedWithViewportIndexing();
+    D3D12_VARIABLE_SHADING_RATE_TIER VariableShadingRateTier = features.VariableShadingRateTier();
+    BOOL ShadingRateImageTileSize = features.ShadingRateImageTileSize();
+    BOOL BackgroundProcessingSupported = features.BackgroundProcessingSupported();
+
+    // 31: Query Metacommand
+
+    // 32: Options7
+    D3D12_MESH_SHADER_TIER MeshShaderTier = features.MeshShaderTier();
+    D3D12_SAMPLER_FEEDBACK_TIER SamplerFeedbackTier = features.SamplerFeedbackTier();
+
+    // 33: Session Type Count
+    UINT SessionTypeCount = features.ProtectedResourceSessionTypeCount();
+    
+    // 34: Session Types
+    std::vector<GUID> SessionTypes = features.ProtectedResourceSessionTypes();
+
+    // 36: Options8
+    BOOL UnalignedBlockTexturesSupported = features.UnalignedBlockTexturesSupported();
+
+    // 37: Options9
+    BOOL MeshShaderPipelineStatsSupported = features.MeshShaderPipelineStatsSupported();
+    BOOL MeshShaderSupportsFullRangeRenderTargetArrayIndex = features.MeshShaderSupportsFullRangeRenderTargetArrayIndex();
+    D3D12_WAVE_MMA_TIER WaveMMATier = features.WaveMMATier();
+
+    // 39: Options10
+    BOOL VariableRateShadingSumCombinerSupported = features.VariableRateShadingSumCombinerSupported();
+    BOOL MeshShaderPerPrimitiveShadingRateSupported = features.MeshShaderPerPrimitiveShadingRateSupported();
+
+    // 40: Options11
+    BOOL AtomicInt64OnDescriptorHeapResourceSupported = features.AtomicInt64OnDescriptorHeapResourceSupported();
 
     return 0;
 }
